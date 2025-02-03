10f4197f04e0add65d749b9399decdb4abea5c7a
DENN <28608482@ppti-14-408-16.ufr-info-p6.jussieu.fr>
2025-02-03 17:38:41 +0100

    endpoints

--- CPS_Marsso_Mougamadoubougary/src/backend/Node.java
+++ CPS_Marsso_Mougamadoubougary/src/backend/Node.java
@@ -1,10 +1,13 @@
 package backend;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import fr.sorbonne_u.components.endpoints.POJOEndPoint;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentAccessSyncI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentDataI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentKeyI;
@@ -13,16 +16,23 @@ import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.MapReduceSyncI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.ProcessorI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.ReductorI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.SelectorI;
+import fr.sorbonne_u.cps.mapreduce.endpoints.POJOContentNodeCompositeEndPoint;
 
 public class Node 
 implements ContentAccessSyncI, MapReduceSyncI{
 
-	public Node (int min, int max) {
+	public Node (int min, int max, POJOContentNodeCompositeEndPoint endPointServer, POJOContentNodeCompositeEndPoint endPointClient) {
 		this.intervalMin = min;
 		this.intervalMax = max;
 		this.tableHachage = new HashMap<Integer, ContentDataI>(max-min);
 		this.memoryTable = new HashMap<>();
-		this.estPasser = false;
+		this.listOfUri = new ArrayList<String>();
+		endPointServer.initialiseServerSide(this);
+		this.mapReduceClient = endPointClient.getMapReduceEndpoint();
+		this.contentAccessClient = endPointClient.getContentAccessEndpoint();
+		this.mapReduceClient.initialiseClientSide(endPointClient);
+		this.contentAccessClient.initialiseClientSide(endPointClient);
+		
 	}
 	
 	private HashMap<Integer, ContentDataI> tableHachage;
@@ -30,15 +40,14 @@ implements ContentAccessSyncI, MapReduceSyncI{
 	private int intervalMin;
 	private int intervalMax;
 	private Node suivant;
-	private boolean estPasser;
+	private POJOEndPoint <MapReduceSyncI> mapReduceClient;
+	private POJOEndPoint <ContentAccessSyncI> contentAccessClient;
+	private List<String> listOfUri;
+
 	
 	
 	public void setSuivant(Node suivant) {
 		this.suivant = suivant; 
-	}
-	
-	public void setEstPasser(boolean estPasser) {
-		this.estPasser = estPasser; 
 	}
 	
 	public boolean contains(ContentKeyI arg0) {
@@ -56,19 +65,21 @@ implements ContentAccessSyncI, MapReduceSyncI{
 
 	@Override
 	public <R extends Serializable> void mapSync(String computationUri, SelectorI selector, ProcessorI<R> processor) throws Exception {
-		if (this.suivant.estPasser) {
-			throw new IllegalArgumentException("La clé n'est pas dans l'intervalle de la table");
-		}
 		this.memoryTable.put(computationUri, ((Stream<ContentDataI>) this.tableHachage.values().stream()
 		.filter(((Predicate<ContentDataI>) selector))
 		.map(processor)));
+		this.listOfUri.add(computationUri);
+		if (this.suivant.listOfUri.contains(computationUri)) {
+			return ;
+		}
 		this.suivant.mapSync(computationUri, selector, processor);
 	}
 
 	@Override
 	public <A extends Serializable, R> A reduceSync(String computationUri, ReductorI<A, R> reductor, CombinatorI<A> combinator, A filteredMap)
 			throws Exception {
-		if (this.suivant.estPasser) {
+		this.listOfUri.add(computationUri);
+		if (this.suivant.listOfUri.contains(computationUri)) {
 			return this.memoryTable.get(computationUri).reduce(filteredMap, (u,d) -> reductor.apply(u,(R) d), combinator);
 		}
 		return combinator.apply(memoryTable.get(computationUri).reduce(filteredMap, (u,d) -> reductor.apply(u,(R) d), combinator), this.suivant.reduceSync(computationUri, reductor, combinator, filteredMap));
@@ -81,36 +92,39 @@ implements ContentAccessSyncI, MapReduceSyncI{
 	}
 
 	@Override
-	public ContentDataI getSync(String attribute, ContentKeyI key) throws Exception {
+	public ContentDataI getSync(String computationUri, ContentKeyI key) throws Exception {
 		
 		if (this.contains(key)) {
 			return tableHachage.get(key.hashCode());
-		} else if(this.suivant.estPasser) {
+		} else if(this.suivant.listOfUri.contains(computationUri)) {
 			throw new IllegalArgumentException("La clé n'est pas dans l'intervalle de la table");
 		} else {
-			return suivant.getSync(attribute, key);
+			this.listOfUri.add(computationUri);
+			return suivant.getSync(computationUri, key);
 		}
 	}
 
 	@Override
-	public ContentDataI putSync(String attribute, ContentKeyI key, ContentDataI data) throws Exception {
+	public ContentDataI putSync(String computationUri, ContentKeyI key, ContentDataI data) throws Exception {
 		if (this.contains(key)) {
 			return tableHachage.put(key.hashCode(), data);
-		} else if(this.suivant.estPasser) {
+		} else if(this.suivant.listOfUri.contains(computationUri)) {
 			throw new IllegalArgumentException("La clé n'est pas dans l'intervalle de la table");
 		} else{
-			return suivant.putSync(attribute, key, data);
+			this.listOfUri.add(computationUri);
+			return suivant.putSync(computationUri, key, data);
 		}
 	}
 
 	@Override
-	public ContentDataI removeSync(String attribute, ContentKeyI key) throws Exception {
+	public ContentDataI removeSync(String computationUri, ContentKeyI key) throws Exception {
 		if (this.contains(key)) {
 			return tableHachage.remove(key.hashCode());
-		} else if(this.suivant.estPasser) {
+		} else if(this.suivant.listOfUri.contains(computationUri)) {
 			throw new IllegalArgumentException("La clé n'est pas dans l'intervalle de la table");
 		}else {
-			return suivant.removeSync(attribute, key);
+			this.listOfUri.add(computationUri);
+			return suivant.removeSync(computationUri, key);
 		}
 	}
 }
--- CPS_Marsso_Mougamadoubougary/src/frontend/Facade.java
+++ CPS_Marsso_Mougamadoubougary/src/frontend/Facade.java
@@ -1,30 +1,41 @@
 package frontend;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.LinkedList;
+import java.util.List;
 
 import backend.Node;
+import fr.sorbonne_u.components.endpoints.POJOEndPoint;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentAccessSyncI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentDataI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentKeyI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.frontend.DHTServicesI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.CombinatorI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.MapReduceSyncI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.ProcessorI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.ReductorI;
 import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.SelectorI;
+import fr.sorbonne_u.cps.mapreduce.endpoints.POJOContentNodeCompositeEndPoint;
 
 public class Facade 
 implements DHTServicesI{
 
-	public Facade (Node anneauNoeud) {
-		this.anneauNoeud = anneauNoeud;
-		this.anneauNoeud.setEstPasser(true);
+	public Facade (POJOContentNodeCompositeEndPoint clientEndPoint) {
+		this.mapReduceClient = clientEndPoint.getMapReduceEndpoint();
+		this.contentAccessClient = clientEndPoint.getContentAccessEndpoint();
+		this.mapReduceClient.initialiseClientSide(clientEndPoint);
+		this.contentAccessClient.initialiseClientSide(clientEndPoint);
 	}
 	
 	private Node anneauNoeud;
+	private POJOEndPoint<ContentAccessSyncI> contentAccessClient;
+	private POJOEndPoint<MapReduceSyncI> mapReduceClient;
+	
 	
 	@Override
 	public ContentDataI get(ContentKeyI key) throws Exception {
-		return this.anneauNoeud.getSync("get", key);
+		return this.contentAccessClient.getClientSideReference().getSync("get", key);
 	}
 
 	@Override
